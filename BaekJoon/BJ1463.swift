3dd9fec7451204360e215b2bbd4d7d72ed3e4d11
JinseoPark <rarebook92@naver.com>
2021-02-04 07:05:44 +0900

    Solved BJ1463
    Studied about TopDown/BottomUP and Memorization.

--- BaekJoon/BJ1463.swift
+++ BaekJoon/BJ1463.swift
@@ -1,21 +1,36 @@
 //https://www.acmicpc.net/problem/1463
+
 import Foundation
-func makeOne(_ v : Int, _ c : Int) -> Int{
-    var count = c
-    if v < 2 {
-        return count
-    }else{
-        count += 1
-        if v % 3 != 0 , v % 2 != 0{
-            return makeOne(v - 1, count)
-        }else if v % 6 == 0{
-            return makeOne(v / 3, count) <= makeOne(v / 2, count) ? makeOne(v / 3, count) : makeOne(v / 2, count)
-        }else if v % 3 == 0{
-            return makeOne(v - 1, count) <= makeOne(v / 3, count) ? makeOne(v - 1, count) : makeOne(v / 3, count)
-        }else{
-            return makeOne(v - 1, count) <= makeOne(v / 2, count) ? makeOne(v - 1, count) : makeOne(v / 2, count)
+//메모이제이션 바텀업
+
+func makeOne(_ input : Int){
+    
+    var dfArr = [Int]()
+    dfArr.append(0) //0부터 시작해서 직관적으로 보고자 넣었다.
+    dfArr.append(0) //1은 그대로 1이기에 카운트 0
+    
+    if input <= 1{
+        print(dfArr[1])
+        return
+    }
+    for i in 2...input{
+        if i%2 != 0 , i%3 != 0{ // 2 , 3으로 안나눠진다는 것은 1을 무조건 빼야함
+            dfArr.append(dfArr[i-1]+1)
+        }else if i%6 == 0{ //6으로 나눠지는것은 3가지 모두 해봐야 한다.
+            let num = dfArr[i/3] <= dfArr[i/2] ? (dfArr[i/3] <= dfArr[i-1] ? dfArr[i/3] : dfArr[i-1] ) : (dfArr[i/2] <= dfArr[i-1] ? dfArr[i/2] : dfArr[i-1] )
+            dfArr.append(num+1)
+        }else if i%3 == 0{ //3은 /3 or -1 비교
+            let num = dfArr[i/3] <= dfArr[i-1] ? dfArr[i/3] : dfArr[i-1]
+            dfArr.append(num+1)
+        }else{ //2는 /2 or -1 비교
+            let num = dfArr[i/2] <= dfArr[i-1] ? dfArr[i/2] : dfArr[i-1]
+            dfArr.append(num+1)
         }
     }
+    print(dfArr[input])
 }
+
+
 let input = Int(readLine()!)!
-print(makeOne(input, 0))
+makeOne(input)
+
