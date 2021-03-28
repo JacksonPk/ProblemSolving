1fe3063c3f9ef5ab6fe721f0283c136d208ead5f
JinseoPark <rarebook92@naver.com>
2021-03-28 15:34:31 +0900

    카카오 인형들
    부르트 포스 문제
    최소 k부터 n까지 모두 구해야한다.

--- BaekJoon/BJ15954.swift
+++ BaekJoon/BJ15954.swift
@@ -1,25 +1,24 @@
 //https://www.acmicpc.net/problem/15954
+
+// 부르트 포스 개념 적용해야함 :  k가 3이 최소일뿐 n까지 모두 봐야한다.
 import Foundation
+
+var min = -1.0
+var arr : [Int] = []
 
 
 func stdev(elements : [Int]) -> Double {
-    let avg = elements.reduce(0, +)/elements.count
+    let avg : Double = Double(elements.reduce(0, +))/Double(elements.count)
     var sumOfDeviation : Double = 0
     var varience : Double = 0
-        
+    
     for element in elements {
-        print("curr elem = ",element)
-        sumOfDeviation += pow(Double((element - avg)), 2)
+        sumOfDeviation += pow(Double((Double(element) - avg)), 2)
     }
     varience = sumOfDeviation / Double(elements.count)
     
-    print("avg : ",avg)
-    print("sumOfDeviation : ",sumOfDeviation)
-    print("varience : ",varience)
     return varience.squareRoot()
 }
-var min = 0
-var arr : [Int] = []
 
 
 let inputNandK = readLine()!.split(separator: " ").compactMap{Int($0)}
@@ -28,22 +27,22 @@ let k = inputNandK[1]
 let inputArr = readLine()!.split(separator: " ").compactMap{Int($0)}
 
 for i in 0...n-k {
-
+    
+    var resultVal = 0.0
     arr = []
-//    print("===============================")
+    
     for j in i..<n {
         arr.append(inputArr[j])
-        
         if arr.count >= k {
-            print(arr)
-            print(stdev(elements: arr))
+            
+            resultVal = stdev(elements: arr)
+            if min == -1 || min > resultVal {
+                
+                min = resultVal
+            }
         }
     }
-//    print(arr)
     
 }
 
-
-// 부르트 포스 개념 적용해야함 :  k가 3이 최소일뿐 n까지 모두 봐야한다.
-//5 3
-//1 2 3 3 1
+print(min)
