c1c1db308a949732c5234390dc518daf9c04fca6
JinseoPark <rarebook92@naver.com>
2021-08-05 20:30:17 +0900

    DP 다시풀어보기

--- BaekJoon/BJ11053.swift
+++ BaekJoon/BJ11053.swift
@@ -1,34 +1,25 @@
-//https://www.acmicpc.net/problem/1932
+//https://www.acmicpc.net/problem/11053
 
 let N = Int(readLine()!)!
-var arr = Array(repeating: [Int](), count: N)
-for i in 0..<N {
-    arr[i] = readLine()!.split(separator: " ").compactMap{Int(String($0))}
-}
-var total = arr
-
-//좌대각 -> total[i][0] = total[i-1][0] + arr[0]
-//우대각 -> total[i][arr[i].count-1] = total[i-1][arr[i].count-1] + [arr[i].count-1]
-//가운대는 max(왼쪽파생, 오른쪽파생)
-//마지막에서 Max(0 ... N)
+let arr = readLine()!.split(separator: " ").compactMap{Int(String($0))}
+var counts = Array(repeating: 1, count: N)
 
 func dp() -> Int {
-    
     if N == 1 {
-        return total[0][0]
+        return 1
     }
     
     for i in 1..<N {
-        
-        total[i][0] = total[i-1][0] + arr[i][0] //왼쪽 끝
-        total[i][total[i].count-1] = total[i-1][total[i-1].count-1] + arr[i][arr[i].count-1] //오른쪽 끝
-                
-        for j in 1..<total[i].count-1 {
-            total[i][j] = max(total[i-1][j-1],total[i-1][j]) + arr[i][j]
+        var count = [1]
+        for j in stride(from: i-1, to: -1, by: -1) { //현재 선택한 값부터 전으로
+            if arr[i] > arr[j]{
+                count.append(counts[j]+1)
+            }
         }
+        counts[i] = count.max()!
     }
-
-    return total[N-1].max()!
+    return counts.max()!
 }
 
 print(dp())
+
