a88c034c6f2c85abf0915ca7b1579122900454a0
JinseoPark <rarebook92@naver.com>
2021-08-16 14:21:33 +0900

    BFS
    메모리와 시간초과를 해결함.
    removeFirst()는 for 안에서 해도 IndexOutOfArray X
    removeFirst()를 대신하기 위해 start,end 변수로 구현 가능

--- BaekJoon/BJ5472.swift
+++ BaekJoon/BJ5472.swift
@@ -1,34 +1,40 @@
 //https://www.acmicpc.net/problem/5427
-//메모리초과 ,, 정답을 봐도 잘 모르겠다
 
-var room: [[Character]] = [[]]
-var pos = [[Int]]()
-var fires = [[Int]]()
-var curFire = [[Int]]()
-var curPos = [[Int]]()
-var count = 1
+//var room: [[String]] = [[String]].init(repeating: [String].init(repeating: " ", count: 1000), count: 1000)
+
+var count = 0
 let dR = [-1,1,0,0]
 let dC = [0,0,-1,1]
-
+var shouldCallFunc = true
+var isEnd = false
+var result = ""
+var pos = [[Int]]()
+var fires = [[Int]]()
+var fs = 0
+var fe = 0
+var ps = 0
+var pe = 0
+var fdiff = 0
+var pdiff = 0
 for _ in 0..<Int(readLine()!)! {
     let nm = readLine()!.split(separator: " ").compactMap{Int(String($0))}
     let n = nm[1]
     let m = nm[0]
-    var shouldCallFunc = true
+    var room: [[String]] = [[String]].init(repeating: [String].init(repeating: " ", count: m), count: n)
+    count = 0
+    shouldCallFunc = true
+    isEnd = false
     pos = []
     fires = []
-    count = 0
-    room = []
-    var isEnd = false
+    
     for i in 0..<n {
         let input = Array(readLine()!)
-        room.append([])
         if shouldCallFunc != false {
             for j in 0..<m {
-                room[i].append(input[j])/// [j] =
+                room[i][j] = String(input[j])
                 switch room[i][j] {
                 case "@":
-                    if (i == 0 || i == n-1 || j == 0 || j == m-1) { shouldCallFunc = false }
+                    if (i == 0 || i == n-1 || j == 0 || j == m-1) { shouldCallFunc = false; break }
                     pos.append([i,j])
                 case "*":
                     fires.append([i,j])
@@ -39,78 +45,63 @@ for _ in 0..<Int(readLine()!)! {
         }
     }
     if !shouldCallFunc { //이미 출구에 있다.
-        print(1)
-        shouldCallFunc = true
+        result.write("1\n")
     } else {
-        while !pos.isEmpty && !isEnd {
-            curFire = fires
-            curPos = pos
-            fires = []//.removeAll()
-            pos = []//.removeAll()
-            
-            curFire.forEach { fire in
+        fs = 0
+        ps = 0
+        fe = fires.count
+        pe = pos.count
+        while ps<pe && !isEnd {
+            fdiff = 0
+            pdiff = 0
+            for i in fs..<fe {
+                let fire = fires[i]
                 let cR = fire[0]
                 let cC = fire[1]
                 for d in 0..<4 {
                     let nR = cR+dR[d]
                     let nC = cC+dC[d]
-                    if (nR >= 0 && nR < n && nC >= 0 && nC < m) && room[nR][nC] == "." {
+                    if (nR >= 0 && nR < n && nC >= 0 && nC < m) && (room[nR][nC] == "." || room[nR][nC] == "@") {
                         room[nR][nC] = "*"
                         fires.append([nR,nC])
+                        fdiff += 1
                     }
                 }
             }
-            for i in 0..<curPos.count {
-                let cR = curPos[i][0]
-                let cC = curPos[i][1]
-                
+            
+            fs = fe
+            fe += fdiff
+            
+            for i in ps..<pe {
+                let curPos = pos[i]
+                let cR = curPos[0]
+                let cC = curPos[1]
+
                 if (cR == 0 || cR == (n-1) || cC == 0 || cC == (m-1)) {
-//                    return count
                     isEnd = true
+                    break
                 }
                 for d in 0..<4 {
                     let nR = cR+dR[d]
                     let nC = cC+dC[d]
                     if (nR >= 0 && nR < n && nC >= 0 && nC < m) && room[nR][nC] == "." {
+                        room[nR][nC] = "@"
                         pos.append([nR,nC])
+                        pdiff += 1
                     }
                 }
             }
+            
+            ps = pe
+            pe += pdiff
+            
             count += 1
         }
-        if !isEnd { //출구 못나옴
-            print("IMPOSSIBLE")
+        if !isEnd {
+            result.write("IMPOSSIBLE\n")
         } else {
-            print(count)
+            result.write("\(count)\n")
         }
-        
-        isEnd = false
     }
 }
-//5
-//4 3
-//####
-//#*.@
-//####
-//7 6
-//###.###
-//#*#.#*#
-//#.....#
-//#.....#
-//#..@..#
-//#######
-//7 4
-//###.###
-//#....*#
-//#@....#
-//.######
-//5 5
-//.....
-//.***.
-//.*@*.
-//.***.
-//.....
-//3 3
-//###
-//#@#
-//###
+print(result)
