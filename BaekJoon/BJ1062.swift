1fed6adb865dde56ec44902e0cfadfe1edca8924
JinseoPark <rarebook92@naver.com>
2021-09-16 22:52:25 +0900

    BitMasking
    시간초과.
    무엇이 문제인지 좀 봐야 할듯

--- BaekJoon/BJ1062.swift
+++ BaekJoon/BJ1062.swift
@@ -9,66 +9,80 @@ import Foundation
 
  모든 단어에서 a,c,i,t,n을 제외한 char를 백트래킹으로 최대한 많이 가질수 있는 것을 이용하면 될듯?
  a,c,i,t,n은 UInt32에서 각각 1로 입력하기. a를 26번째 자리로 넣기.
+
+ 캐릭터들을 Ascii로 변환해서 처리하기.
  */
 
 let input = readLine()!.split(separator: " ").compactMap{Int(String($0))}
 let (N,K) = (input[0],input[1])
-var charSet:Set<Character> = Set()
+var addedBitSet:Set<UInt32> = Set()
 var defaultChars: [Character] = ["a","c","i","n","t"]
-var word: [Character]
-var allWords: []
-var isRequired = true
-var minBit: UInt32 = 0
+var defaultBits: [UInt32] = []
+var allWords: [UInt32] = []
+var initBit: UInt32 = 0 //["a","c","i","n","t"] 가 들어있는 bit
 let standardChar: Character = defaultChars[0] //a를 기준으로 몇칸 떨어져 있는지 알기위해서
-let standardIdx: UInt8 = 25
+let standardIdx: UInt8 = 25 //알파벳 수 기준 Idx
+var shiftbit:UInt32 = 0
+var isRequired = true
+
+//Init Bit, append [a,c,i,n,t]
+for ch in defaultChars {
+    defaultBits.append(1<<(standardIdx - (ch.asciiValue! - standardChar.asciiValue!)))
+    initBit |= defaultBits.last!
+}
 
 for _ in 0..<N {
-    word = Array(readLine()!)
-    for i in 4..<word.count-4 { //"anta" "tica"를 뺀 중간값만 확인
+    let word = Array(readLine()!)
+    var bit:UInt32 = 0
+    
+    for i in 0..<word.count {
         isRequired = true
-        for j in 0..<defaultChars.count where defaultChars[j] == word[i] {
-            isRequired = false //중간 값에 "a,c,i,n,t가 있으면 넣지 않는다.
+        shiftbit = UInt32(standardIdx - (word[i].asciiValue! - standardChar.asciiValue!))
+        bit |= 1<<shiftbit
+        for j in 0..<defaultBits.count where (defaultBits[j] == 1<<shiftbit) {
+            isRequired = false //"a,c,i,n,t가 있으면 넣지 않는다.
         }
         if isRequired {
-            charSet.insert(word[i])
+            addedBitSet.insert(shiftbit)
         }
     }
+    allWords.append(bit) //해당 단어의 2진수를 넣는다.
 }
 
 func getTotalPossible(arr: [UInt32]) -> Int {
     var count = 0
-    var nbit = minBit
+    var nBit = initBit
 
+    //백트래킹으로 가져온 비트들을 더하는 작업
     for i in 0..<arr.count {
-        nbit |= 1<<(arr[i])
+        nBit |= 1<<(arr[i])
     }
-
-    for i in 0..<
-
+    //OR연산해도 nBit면 word는 nBit에 포함관계
+    for word in allWords where nBit == (word | nBit) {
+        count += 1
+    }
+    
     return count
 }
 
-func bitMasking(chars: [Character]) -> Int {
+func bitMasking(chars: [UInt32]) -> Int {
     var maxCount = 0
     var isUsed = Array(repeating: false, count: chars.count)
     var arr = [UInt32]()
 
-//    print("char",chars)
-//    for i in 0..<chars.count {
-//        print(UInt32(standardIdx - (chars[i].asciiValue! - standardChar.asciiValue!)))
-//    }
     if K < 5 {
         return maxCount
     }
 
-    func backTracking(idx: Int) {
+    func backTracking(idx: Int) { //nCr 백트래킹
         if arr.count == K-5 {
-            print(arr)
+            //입력된 문자bits를 가장 많이 포함된 bit의 값 저장
+            maxCount = max(maxCount,getTotalPossible(arr: arr))
             return
         }
         for i in idx..<chars.count where !isUsed[i] {
             isUsed[i] = true
-            arr.append(UInt32(standardIdx - (chars[i].asciiValue! - standardChar.asciiValue!)))
+            arr.append(chars[i])
             backTracking(idx: idx+1)
             isUsed[i] = false
             arr.removeLast()
@@ -79,13 +93,6 @@ func bitMasking(chars: [Character]) -> Int {
     return maxCount
 }
 
-//Init minBit
-for ch in defaultChars {
-    minBit |= 1<<(standardIdx - (ch.asciiValue! - standardChar.asciiValue!))
-}
-//print(minBit)
-//print(String(minBit,radix: 2))
-
-print(bitMasking(chars: Array(charSet)))
+print(bitMasking(chars: Array(addedBitSet)))
 
 
